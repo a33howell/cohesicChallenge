910b1932ff18e33ccce838e32e8505b776e99ffe
a33howell <andrew33.howell@gmail.com>
2018-10-21 16:35:05 -0600

    Select and deselect implemented

--- src/cohesicChallenge/AnatomicalHierarchy.java
+++ src/cohesicChallenge/AnatomicalHierarchy.java
@@ -1,4 +1,5 @@
 package cohesicChallenge;
+import java.io.*;
 
 /**
  * @author      Andrew Howell
@@ -13,10 +14,9 @@ public class AnatomicalHierarchy {
 	 * Chest includes lungs and heart hierarchies
 	 */
 	public AnatomicalHierarchy() {
-		root = new Node("Chest");
-		Node lungs = initializeLungs();
-		Node heart = initializeHeart();
-		root.setDescendants(new Node[]{lungs, heart});
+		root = new Node("Chest", new Node[]{
+				initializeLungs(),
+				initializeHeart()});
 	}
 	
 	
@@ -27,21 +27,18 @@ public class AnatomicalHierarchy {
 	 */
 	private Node initializeLungs() {
 		// Create the right lung Node and its descendants
-		Node rightLung = new Node("Right Lung");
-		rightLung.setDescendants(new Node[]{
-					new Node("Superior Lobe"),
-					new Node("Middle Lobe"),
-					new Node("Inferior Lobe")});
+		Node rightLung = new Node("Right Lung", new Node[]{
+					new Node("R Superior Lobe", null),
+					new Node("R Middle Lobe", null),
+					new Node("R Inferior Lobe", null)});
 		
 		// Create the left lung Node and its descendants
-		Node leftLung = new Node("Left Lung");
-		leftLung.setDescendants(new Node[]{
-				new Node("Superior Lobe"),
-				new Node("Inferior Lobe")});
+		Node leftLung = new Node("Left Lung", new Node[]{
+				new Node("L Superior Lobe", null),
+				new Node("L Inferior Lobe", null)});
 		
 		// Combine left and right lungs into "lungs" hierarchy
-		Node lungs = new Node("Lungs");
-		lungs.setDescendants(new Node[]{rightLung, leftLung});
+		Node lungs = new Node("Lungs", new Node[]{rightLung, leftLung});
 		
 		return lungs;
 	}
@@ -54,15 +51,26 @@ public class AnatomicalHierarchy {
 	 */
 	private static Node initializeHeart() {
 		// Create heart Node and its descendants
-		Node heart = new Node("Heart");
-		heart.setDescendants(new Node[]{
-					new Node("Left Ventricle"),
-					new Node("Right Ventricle"),
-					new Node("Left Aorta"),
-					new Node("Right Aorta"),
-					new Node("Septum")});
+		Node heart = new Node("Heart", new Node[]{
+					new Node("Left Ventricle", null),
+					new Node("Right Ventricle", null),
+					new Node("Left Aorta", null),
+					new Node("Right Aorta", null),
+					new Node("Septum", null)});
 		
 		return heart;
+	}
+	
+	
+	
+	/**
+	 * Prints error message to the terminal when user provides invalid input
+	 */
+	private static void printErrorMessage() {
+		System.out.println("Invalid input");
+		System.out.println("Input must have the form \"select/deselect [node name]\"");
+		System.out.println("Input \"END\" to terminate the program");
+		System.out.println();
 	}
 	
 	
@@ -75,5 +83,53 @@ public class AnatomicalHierarchy {
 		//Create a new chest hierarchy and print its info to the terminal
 		AnatomicalHierarchy hierarchy = new AnatomicalHierarchy();
 		hierarchy.root.printHierarchy();
+		
+		//Create new buffer reader for user command line input
+		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+		String input = "";
+		
+		while(!input.equals("END")) {
+			try {
+				//Take user input
+				System.out.print("Input: ");
+				input = bufferReader.readLine();
+				System.out.println();
+				
+				//Terminate program if user inputs "END"
+				if (input.equals("END")) return;
+				
+				//Print error message if user provides invalid input
+				String[] inputSplit = input.split(" ", 2);
+				if (inputSplit.length != 2) {
+					printErrorMessage();
+					continue;
+				}
+				
+				//Search for Node with given name
+				//If no such Node exists, then print an error message
+				Node node = root.nodeSearch(inputSplit[1]);
+				if (node == null) {
+					System.out.println("\"" + inputSplit[1] + "\" not found");
+					continue;
+				}
+				
+				//Select specified Node
+				if (inputSplit[0].equals("select")) node.select();
+				//Deselect specified Node
+				else if (inputSplit[0].equals("deselect")) node.deselect();
+				//Print error message if user provides invalid input
+				else {
+					printErrorMessage();
+					continue;
+				}
+				
+				//Print updated hierarchy
+				hierarchy.root.printHierarchy();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/cohesicChallenge/Node.java
+++ src/cohesicChallenge/Node.java
@@ -12,12 +12,18 @@ public class Node {
 	
 	/**
 	 * Constructor
+	 * Sets name and descendants of the Node
+	 * !!! Ensure that nodes in a hierarchy have unique names
+	 * !!! Current version does not support searching for Nodes with same names
 	 * @param name - name of node
 	 */
-	public Node(String name) {
+	public Node(String name, Node[] descendants) {
 		this.name = name;
-		this.ancestor = ancestor;
 		this.descendants = descendants;
+		
+		//Update the ancestor pointers of the descendants (if any)
+		if (descendants != null)
+			for(Node descendant: descendants) descendant.setAncestor(this);
 	}
 	
 	
@@ -25,11 +31,7 @@ public class Node {
 	/**
 	 * @param ancestor - parent of the current Node
 	 */
-	public void setAncestor(Node ancestor) {this.ancestor = ancestor;}
-	/**
-	 * @param descendants - children of the current Node
-	 */
-	public void setDescendants(Node[] descendants) {this.descendants = descendants;}
+	private void setAncestor(Node ancestor) {this.ancestor = ancestor;}
 	/**
 	 * @return Name of the current Node
 	 */
@@ -46,6 +48,28 @@ public class Node {
 	
 	
 	/**
+	 * Inspects the current Node and its descendants to find a Node with a particular
+	 * @param name - name of the Node that we want to find
+	 * @return The Node with the same name as the parameter (returns null if no such Node exists)
+	 */
+	public Node nodeSearch(String name) {
+		//Return the current Node if it has a matching name
+		if(this.name.equals(name)) return this;
+		//If current node isn't a match, then search its descendants
+		else if (descendants != null){
+			//Loop/recurse through each descendant until a match is found
+			for(Node descendant: descendants) {
+				Node foundNode = descendant.nodeSearch(name);
+				if(foundNode != null) return foundNode;
+			}
+		}
+		//If no match is found, then return null
+		return null;
+	}
+	
+	
+	
+	/**
 	 * Sets current Node as "selected", as well as its ancestors
 	 */
 	public void select() {
@@ -58,7 +82,8 @@ public class Node {
 	 */
 	public void deselect() {
 		selected = false;
-		if(descendants != null) for(Node descendant: descendants) descendant.deselect();
+		if(descendants != null)
+			for(Node descendant: descendants) descendant.deselect();
 	}
 	
 	
@@ -70,10 +95,13 @@ public class Node {
 		//Print info for root Node
 		printNodeInfo();
 		//Print info for descendants (if any)
-		if(descendants != null) for(Node descendant: descendants) printHierarchy1(descendant, 1);
+		if(descendants != null)
+			for(Node descendant: descendants) printHierarchy1(descendant, 1);
+		System.out.println();
 	}
 	
 	/**
+	 * Complimentary method for printHierarchy()
 	 * Prints a sub-Node within a greater hierarchy
 	 * @param node - Current Node in the hierarchy we wish to print
 	 * @param height - Height of the current Node in the hierarchy tree
@@ -85,17 +113,20 @@ public class Node {
 			System.out.print("\t");
 			temp--;
 		}
+		
 		//Print info for current Node
 		node.printNodeInfo();
 		//Print info for descendants (if any)
 		int descendantHeight = height + 1;
-		if(node.getDescendants() != null) for(Node descendant: node.getDescendants()) printHierarchy1(descendant, descendantHeight);
+		if(node.getDescendants() != null)
+			for(Node descendant: node.getDescendants()) printHierarchy1(descendant, descendantHeight);
 	}
 	
 	/**
 	 * Prints relevant data of the Node to the terminal
 	 */
 	private void printNodeInfo() {
+		//If node is selected, then print an 'X' to the terminal
 		String temp = " ";
 		if (selected) temp = "X";
 		System.out.println("[" + temp + "] " + name);
